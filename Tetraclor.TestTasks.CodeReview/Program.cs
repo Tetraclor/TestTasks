c1e266044f07792aa18a8545fbcca0f275d93e5f
Даниил Королев <kdvikt@gmail.com>
2022-04-30 13:35:48 +0400

    CodeReview bag fix

--- Tetraclor.TestTasks.CodeReview/Program.cs
+++ Tetraclor.TestTasks.CodeReview/Program.cs
@@ -20,7 +20,15 @@ namespace Tetraclor.TestTasks.CodeReview
     {
         static void Main(string[] args)
         {
-            SomeClass.GetAttributeValueFromXmlFile("example.xml", "", "");
+            var xmlFile = "example.xml";
+            var elementName = "ingredient";
+            var attrName = "unit";
+
+            var func1Result = SomeClass.Func1(xmlFile, elementName, attrName);
+            var refactResult = SomeClass.GetAttributeValueFromXmlFile(xmlFile, elementName, attrName);
+            var useRegexResult = SomeClass.GetAttributeValueFromXmlFile_UseRegex(xmlFile, elementName, attrName);
+            var useXmlParserResult = SomeClass.GetAttributeValueFromXmlFile_UseXmlParser(xmlFile, elementName, attrName);
+            ;
         }
     }
 
@@ -29,11 +37,15 @@ namespace Tetraclor.TestTasks.CodeReview
         // 0. Считываются возможные закоментированные строки, значение будет считано из комментариев, что является ошибочным поведением
         // 0. Функция считывает только первое возможное значение
         // 0. Функция считает значение даже если xml не валиден
-        // 0. Функция не обрабатывает невалидные значения входных параметров, такие как пустые строки и null
+        // 0. Функция не обрабатывает не валидные значения входных параметров, такие как пустые строки и null
+        // Возможно замечания не имеют оснований если гарантируется
+        // что входные параметры будут валидными и файл xml будет существовать и будет доступен
+        //
         // 1. Неплохо бы иметь Summury у функции
         // 2. Func1 ничего не говорит о том что делает эта функция
         // 3. Название входного параметра input не очевидно,
         // хотя должно указывать на то что это путь до файла xml
+        // 4. attr 
         // Также можно запутаться с порядком параметроф функции, так как они имеют один и тот же тип string
         // Решение: передача объекта, FluentAPI, но возомжно это излишне
         public static string Func1(string input, string elementName, string attrName)
@@ -43,7 +55,8 @@ namespace Tetraclor.TestTasks.CodeReview
             string[] lines = System.IO.File.ReadAllLines(input);
             // Можно назвать attrValue
             string result = null;
-
+            // Так как анализ xml документа проходит по линиям,
+            // то в случае когда элемент и его атрибут будут находиться на разных линиях данная функция не сработает
             foreach (var line in lines)
             {
                 // 6. Опечатка с наименованием startElEndex - Index, сокращение El - лучше писать польностью
@@ -65,10 +78,10 @@ namespace Tetraclor.TestTasks.CodeReview
                         if (attrStartIndex != -1)
                         {
                             // 12. Тут неплохо бы вставить комментарий смысла этого выражения и числа 2
-                            // int вместо var
+                            // int вместо ожидаемого var
                             int valueStartIndex = attrStartIndex + attrName.Length + 2;
 
-                            // 13. Почему не использовать IndexOf(line, valueStartIndex + 1)
+                            // 13. Почему не использовать IndexOf('"', line, valueStartIndex + 1)
                             while (line[valueStartIndex] != '"')
                             {
                                 // 14. Если следовать пункту 13 лучше использовать Substring
@@ -86,16 +99,16 @@ namespace Tetraclor.TestTasks.CodeReview
             return result;
         }
 
-        // Пример где исправлено только оформление кода
+        // Рефакторинг без изменения поведения функции
 
         /// <summary>
         ///  Функция выполняет поиск значения переданного атрибута элемента Xml-документа.
         /// </summary>
         /// <param name="pathToXmlFile">путь до файла</param>
         /// <param name="elementName">название элемента (в котором ищется значение);</param>
-        /// <param name="attrName">название атрибута</param>
+        /// <param name="attributeName">название атрибута</param>
         /// <returns>значение атрибута</returns>
-        public static string GetAttributeValueFromXmlFile(string pathToXmlFile, string elementName, string attrName)
+        public static string GetAttributeValueFromXmlFile(string pathToXmlFile, string elementName, string attributeName)
         {
             var lines = File.ReadAllLines(pathToXmlFile);
             string attrValue = null;
@@ -104,20 +117,20 @@ namespace Tetraclor.TestTasks.CodeReview
             {
                 var startElementIndex = line.IndexOf(elementName);
 
-                if (startElementIndex == 1)
+                if (startElementIndex == -1)
                     continue;
 
                 if (line[startElementIndex - 1] != '<')
                     continue;
                 
                 var endElementIndex = line.IndexOf('>', startElementIndex - 1);
-                var startAttrIndex = line.IndexOf(attrName, startElementIndex, endElementIndex - startElementIndex + 1);
+                var startAttrIndex = line.IndexOf(attributeName, startElementIndex, endElementIndex - startElementIndex + 1);
 
                 if (startAttrIndex == -1)
                     continue;
 
                 // От начала атрибута пропускаем его длину символ = и символ "
-                var valueStartIndex = startAttrIndex + attrName.Length + 2; 
+                var valueStartIndex = startAttrIndex + attributeName.Length + 2; 
 
                 while (line[valueStartIndex] != '"')
                 {
@@ -132,11 +145,11 @@ namespace Tetraclor.TestTasks.CodeReview
         }
 
         // Пример альтернативной возможной реализации
-        public static string GetAttributeValueFromXmlFile_UseRegex(string pathToXmlFile, string elementName, string attrName)
+        public static string GetAttributeValueFromXmlFile_UseRegex(string pathToXmlFile, string elementName, string attributeName)
         {
             var xmlText = File.ReadAllText(pathToXmlFile);
 
-            var regex = new Regex(@"<\s*" + elementName + @"\s[^>]+\" + attrName + @"\s*=\s*""");
+            var regex = new Regex(@"<\s*" + elementName + @"\b[^>]+\b" + attributeName + @"\s*=\s*""");
             var match = regex.Match(xmlText);
 
             if (match.Success == false)
@@ -147,19 +160,24 @@ namespace Tetraclor.TestTasks.CodeReview
             if (closeQuotesIndex == -1)
                 return null;
 
-            var attrValue = xmlText.Substring(match.Index, closeQuotesIndex);
+            var attributeValueStartIndex = match.Index + match.Length;
+            var attributeValue = xmlText[attributeValueStartIndex..closeQuotesIndex];
 
-            return attrValue;
+            return attributeValue;
         }
 
         // Лучше всего использовать xml parser
-        public static string GetAttributeValueFromXmlFile_UseXmlParser(string pathToXmlFile, string elementName, string attrName)
+        public static string GetAttributeValueFromXmlFile_UseXmlParser(string pathToXmlFile, string elementName, string attributeName)
         {
             var xml = new XmlDocument();
             xml.Load(pathToXmlFile);
 
-            var value = xml.SelectNodes($"//{elementName}/@{attrName}");
-            return null;
+            var attributesValues = xml.SelectNodes($"//{elementName}/@{attributeName}");
+            
+            if(attributesValues.Count == 0)
+                return null;
+
+            return attributesValues[0].Value;
         }
     }
 }
